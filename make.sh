0966d19de62390eb55ffb54c40a048fb05990e1a
mechairoi <ttsujikawa@gmail.com>
2015-12-03 05:00:18 +0900

    wip

--- make.sh
+++ make.sh
@@ -2,5 +2,6 @@ git submodule init
 cd purescript-hatter
 npm install
 bower install
-$(npm bin)/gulp pack
-cp hatter.js ../
+$(npm bin)/gulp bin
+cp index.js ../hatter.js
+echo "module.exports = PS['Text.Hatter'].hatter;" >> ../hatter.js
